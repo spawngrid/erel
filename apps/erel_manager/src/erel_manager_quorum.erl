aaee0428eaeab15904e0103fa83fcb2ac3981a6d
Yurii Rashkovskii <yrashk@spawngrid.com>
2011-10-26 04:41:47 -0700

    Makes erel_manager_quorum a little bit more flexible so it can be used for other quorum polls

--- apps/erel_manager/src/erel_manager_quorum.erl
+++ apps/erel_manager/src/erel_manager_quorum.erl
@@ -1,31 +1,35 @@
 -module(erel_manager_quorum).
--export([start/3, stop/2]).
+-export([start/3, start/5, stop/2]).
 -export([binding/1, init/2, handle_message/2, handle_cast/2, handle_call/3]).
 
 -include_lib("erel_manager/include/erel_manager.hrl").
 
--record(state, { endpoint :: pid(),  expected = [] :: list(string()), cb :: fun(() -> any()) }).
+-record(state, { endpoint :: pid(),  expected = [] :: list(string()), cb :: fun(() -> any()),
+                 reply :: term()  }).
 
 start(Topic, Hosts, Fun) ->
+  start(ping, pong, Topic, Hosts, Fun).
+
+start(Req, Reply, Topic, Hosts, Fun) ->
   {ok, Endpoint} = application:get_env(erel_manager, endpoint),  
   supervisor:start_child(erel_manager_quorum_sup, 
-    esupervisor:spec(?endp_worker(Endpoint, {Topic, Hosts}, ?MODULE, {Topic, Hosts, Fun}))).
+    esupervisor:spec(?endp_worker(Endpoint, {Topic, Hosts}, ?MODULE, {Req, Reply,Topic, Hosts, Fun}))).
 
 stop(Topic, Hosts) ->
   supervisor:terminate_child(erel_manager_quorum_sup, {Topic, Hosts}),
   supervisor:delete_child(erel_manager_quorum_sup, {Topic, Hosts}).
 
-binding({Topic, _Expected, _Fun}) ->
+binding({_Req, _Reply, Topic, _Expected, _Fun}) ->
   #endp_binding{ name = erel, type = topic, topic = Topic }.
 
-init(Endpoint, {Topic, Expected, Fun}) ->
-  erel_endp:cast(Endpoint, erel, Topic, ping),
-  {ok, #state{ endpoint = Endpoint, expected = Expected, cb = Fun }}.
+init(Endpoint, {Req, Reply, Topic, Expected, Fun}) ->
+  Req =/= none andalso erel_endp:cast(Endpoint, erel, Topic, Req),
+  {ok, #state{ endpoint = Endpoint, expected = Expected, cb = Fun, reply = Reply }}.
 
-handle_message({announce, Hostname}, #state{} = State) ->
-  handle_message({pong, Hostname}, State);
+handle_message({announce, Hostname}, #state{ reply = Reply } = State) ->
+  handle_message({Reply, Hostname}, State);
 
-handle_message({pong, Hostname}, #state{ expected = Expected, cb = Cb } = State) ->
+handle_message({Reply, Hostname}, #state{ expected = Expected, cb = Cb, reply = Reply } = State) ->
   Expected1 = Expected -- [Hostname],
   ?INFO("Host ~s is alive, left to join: ~p",[Hostname, Expected1]),
   case Expected1 of 
